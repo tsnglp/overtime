f7f90bcfd264acaf3e976c735471bc680cc0032c
Tsengel Purevdorj <tsnglp@gmail.com>
2020-08-04 16:47:10 +1000

    Add rspec and setup homepage

---
+++ spec/rails_helper.rb
@@ -0,0 +1,27 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../config/environment', __dir__)
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
+require 'rspec/rails'
+require 'capybara/rails'
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = false
+  config.before(:suite) {DatabaseCleaner.clean_with(:truncation)}
+  config.before(:each) {DatabaseCleaner.strategy = :transaction}
+  config.before(:each, :js => true) {DatabaseCleaner.strategy = :truncation}
+  config.before(:each) {DatabaseCleaner.start}
+  config.after(:each) {DatabaseCleaner.clean}
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+end
